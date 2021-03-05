ced833b8d724290f87f45bc318790a75463f077a
ANKIT MEHRA <ankitmehra86@users.noreply.github.com>
2021-03-04 20:08:07 -0500

    test commit

--- Assignment2Group11.cs
+++ Assignment2Group11.cs
@@ -6,6 +6,10 @@ namespace EmployeeTest
     {
         static void Main(string[] args)
         {
+
+            employee createNew = new employee();
+            employee createNewStats = new employee(007,"James Bond",2500)
+
         }
 
     }
