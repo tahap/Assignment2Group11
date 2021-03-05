40d7937211895bc91338f186d9ea4f701c511768
tahap <79608998+tahap@users.noreply.github.com>
2021-03-04 19:20:07 -0500

    Update Assignment2Group11.cs

--- Assignment2Group11.cs
+++ Assignment2Group11.cs
@@ -6,6 +6,8 @@ namespace EmployeeTest
     {
         static void Main(string[] args)
         {
+            employee createNew = new employee();
+            employee createNewStats = new employee(007,"James Bond",2500)
         }
     }
 }
